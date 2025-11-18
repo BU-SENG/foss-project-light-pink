ec85c653ad87efd063baf1afdad37dd0b3eb42ef
Ajala Jeremiah <123889766+jerex23@users.noreply.github.com>
2025-11-19 00:09:27 +0100

    added the ui.types file and created index.ts

---
+++ src/types/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Central export file for all TypeScript types and interfaces used throughout the application.
+ */
+export * from './code.types';
+export * from './api.types';
+export * from './auth.types';
+export * from './database.types';
+export * from './ui.types';
